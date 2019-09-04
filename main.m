7a510e1f83aabfc833af16554b9c9e00fbb88fa7
Guilherme <gfs8@duke.edu>
2019-09-04 14:37:17 -0400

    Add path to Data folder.

--- main.m
+++ main.m
@@ -1,5 +1,8 @@
 %% Change Path and Defaults for Plots
 addpath('functions', 'scripts');
+% path to data folder that sits outside the project folder
+% ".." means parent folder
+addpath(fullfile('..', 'Data'));
 plot_defaults;
 
 %%
